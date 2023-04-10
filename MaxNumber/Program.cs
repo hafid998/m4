1e77611d11da6916244ef69f977f0aced239af6d
hafid998 <sidiq0304@students.amikom.ac.id>
2023-04-10 22:34:20 +0700

    minggu 5

---
+++ MaxNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using MaxNumber;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            number numbers = new number();
+            numbers.CariNilaiMin(12, 76);
+            numbers.CariNilaiMin(18, 49, 89);
+            numbers.CariNilaiMax(87, 99);
+            numbers.CariNilaiMax(35, 23, 45);
+        }
+    }
+}
